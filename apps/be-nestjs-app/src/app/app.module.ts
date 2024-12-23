0dab80d85dc60f2844e3680d55674cbe7f094734
ThanhTNV <thanhtran190304@gmail.com>
2024-12-23 16:46:20 +0700

    feat: authenticate, prisma schema, utils, decorators, database service, user main features

--- apps/be-nestjs-app/src/app/app.module.ts
+++ apps/be-nestjs-app/src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { JwtUtilsModule } from './utils/jwt/jwtUtils.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
